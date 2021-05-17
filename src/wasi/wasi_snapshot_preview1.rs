a904e1ff5dc2bdc91fbda9b7f5a0a25ea0d5d928
cohyou <cohyou@ironoir.io>
2021-05-18 02:07:40 +0900

    add newlines to func doc

--- src/wasi/wasi_snapshot_preview1.rs
+++ src/wasi/wasi_snapshot_preview1.rs
@@ -29,93 +29,192 @@ use crate::wasi::{
     Subscription,
 };
 
-/// args_get(argv: Pointer<Pointer<u8>>, argv_buf: Pointer<u8>) -> Result<(), errno>
+/// Read command-line argument data. 
+/// The size of the array should match that returned by args_sizes_get. 
+/// Each argument is expected to be \0 terminated.
 fn args_get(argv: *mut *mut u8, argv_buf: *mut u8) -> Result<(), Errno> { unimplemented!() }
-/// args_sizes_get() -> Result<(size, size), errno>
+
+/// Return command-line argument data sizes.
 fn args_sizes_get() -> Result<(Size, Size), Errno> { unimplemented!() }
-/// environ_get(environ: Pointer<Pointer<u8>>, environ_buf: Pointer<u8>) -> Result<(), errno>
+
+/// Read environment variable data.
+/// The sizes of the buffers should match that returned by environ_sizes_get.
+/// Key/value pairs are expected to be joined with =s, and terminated with \0s.
 fn environ_get(environ: *mut *mut u8, environ_buf: *mut u8) -> Result<(), Errno> { unimplemented!() }
-/// environ_sizes_get() -> Result<(size, size), errno>
+
+/// Return environment variable data sizes.
 fn environ_sizes_get() -> Result<(Size, Size), Errno> { unimplemented!() }
-/// clock_res_get(id: clockid) -> Result<timestamp, errno>
+
+/// Return the resolution of a clock. 
+/// Implementations are required to provide a non-zero value for supported clocks. 
+/// For unsupported clocks, return errno::inval. 
+/// Note: This is similar to clock_getres in POSIX.
 fn clock_res_get(id: Clockid) -> Result<Timestamp, Errno> { unimplemented!() }
-/// clock_time_get(id: clockid, precision: timestamp) -> Result<timestamp, errno>
+
+/// Return the time value of a clock. Note: This is similar to clock_gettime in POSIX.
 fn clock_time_get(id: Clockid, precision: Timestamp) -> Result<Timestamp, Errno> { unimplemented!() }
-/// fd_advise(fd: fd, offset: filesize, len: filesize, advice: advice) -> Result<(), errno>
+
+/// Provide file advisory information on a file descriptor. 
+/// Note: This is similar to posix_fadvise in POSIX.
 fn fd_advise(fd: Fd, offset: Filesize, len: Filesize, advice: Advice) -> Result<(), Errno> { unimplemented!() }
-/// fd_allocate(fd: fd, offset: filesize, len: filesize) -> Result<(), errno>
+
+/// Force the allocation of space in a file. 
+/// Note: This is similar to posix_fallocate in POSIX.
 fn fd_allocate(fd: Fd, offset: Filesize, len: Filesize) -> Result<(), Errno> { unimplemented!() }
-/// fd_close(fd: fd) -> Result<(), errno>
+
+/// Close a file descriptor. 
+/// Note: This is similar to close in POSIX.
 fn fd_close(fd: Fd) -> Result<(), Errno> { unimplemented!() }
-/// fd_datasync(fd: fd) -> Result<(), errno>
+
+/// Synchronize the data of a file to disk. 
+/// Note: This is similar to fdatasync in POSIX.
 fn fd_datasync(fd: Fd) -> Result<(), Errno> { unimplemented!() }
-/// fd_fdstat_get(fd: fd) -> Result<fdstat, errno>
+
+/// Get the attributes of a file descriptor. 
+/// Note: This returns similar flags to fsync(fd, F_GETFL) in POSIX, as well as additional fields.
 fn fd_fdstat_get(fd: Fd) -> Result<Fdstat, Errno> { unimplemented!() }
-/// fd_fdstat_set_flags(fd: fd, flags: Fdflags) -> Result<(), errno>
+
+/// Adjust the flags associated with a file descriptor. 
+/// Note: This is similar to fcntl(fd, F_SETFL, flags) in POSIX.
 fn fd_fdstat_set_flags(fd: Fd, flags: Fdflags) -> Result<(), Errno> { unimplemented!() }
-/// fd_fdstat_set_rights(fd: fd, fs_rights_base: rights, fs_rights_inheriting: rights) -> Result<(), errno>
+
+/// Adjust the rights associated with a file descriptor. 
+/// This can only be used to remove rights, and returns errno::notcapable if called in a way that would attempt to add rights
 fn fd_fdstat_set_rights(fd: Fd, fs_rights_base: Rights, fs_rights_inheriting: Rights) -> Result<(), Errno> { unimplemented!() }
-/// fd_filestat_get(fd: fd) -> Result<filestat, errno>
+
+/// Return the attributes of an open file.
 fn fd_filestat_get(fd: Fd) -> Result<Filestat, Errno> { unimplemented!() }
-/// fd_filestat_set_size(fd: fd, size: filesize) -> Result<(), errno>
+
+/// Adjust the size of an open file. 
+/// If this increases the file's size, the extra bytes are filled with zeros. 
+/// Note: This is similar to ftruncate in POSIX.
 fn fd_filestat_set_size(fd: Fd, Size: Filesize) -> Result<(), Errno> { unimplemented!() }
-/// fd_filestat_set_times(fd: fd, atim: timestamp, mtim: timestamp, fst_flags: fstflags) -> Result<(), errno>
+
+/// Adjust the timestamps of an open file or directory. 
+/// Note: This is similar to futimens in POSIX.
 fn fd_filestat_set_times(fd: Fd, atim: Timestamp, mtim: Timestamp, fst_flags: Fstflags) -> Result<(), Errno> { unimplemented!() }
-/// fd_pread(fd: fd, iovs: iovec_array, offset: filesize) -> Result<size, errno>
+
+/// Read from a file descriptor, without using and updating the file descriptor's offset. 
+/// Note: This is similar to preadv in POSIX.
 fn fd_pread(fd: Fd, iovs: IovecArray, offset: Filesize) -> Result<Size, Errno> { unimplemented!() }
-/// fd_prestat_get(fd: fd) -> Result<prestat, errno>
+
+/// Return a description of the given preopened file descriptor.
 fn fd_prestat_get(fd: Fd) -> Result<Prestat, Errno> { unimplemented!() }
-/// fd_prestat_dir_name(fd: fd, path: Pointer<u8>, path_len: size) -> Result<(), errno>
+
+/// Return a description of the given preopened file descriptor.
 fn fd_prestat_dir_name(fd: Fd, path: *mut u8, path_len: Size) -> Result<(), Errno> { unimplemented!() }
-/// fd_pwrite(fd: fd, iovs: ciovec_array, offset: filesize) -> Result<size, errno>
+
+/// Write to a file descriptor, without using and updating the file descriptor's offset. 
+/// Note: This is similar to pwritev in POSIX.
 fn fd_pwrite(fd: Fd, iovs: CiovecArray, offset: Filesize) -> Result<Size, Errno> { unimplemented!() }
-/// fd_read(fd: fd, iovs: iovec_array) -> Result<size, errno>
+
+/// Read from a file descriptor. 
+/// Note: This is similar to readv in POSIX.
 fn fd_read(fd: Fd, iovs: IovecArray) -> Result<Size, Errno> { unimplemented!() }
-/// fd_readdir(fd: fd, buf: Pointer<u8>, buf_len: size, cookie: dircookie) -> Result<size, errno>
+
+/// Read directory entries from a directory. 
+/// When successful, the contents of the output buffer consist of a sequence of directory entries. 
+/// Each directory entry consists of a dirent object, followed by dirent::d_namlen bytes holding the name of the directory entry. 
+/// This function fills the output buffer as much as possible, potentially truncating the last directory entry. 
+/// This allows the caller to grow its read buffer size in case it's too small to fit a single large directory entry, or skip the oversized directory entry.
 fn fd_readdir(fd: Fd, buf: *mut u8, buf_len: Size, cookie: Dircookie) -> Result<Size, Errno> { unimplemented!() }
-/// fd_renumber(fd: fd, to: Fd) -> Result<(), errno>
+
+/// Atomically replace a file descriptor by renumbering another file descriptor. 
+/// Due to the strong focus on thread safety, this environment does not provide a mechanism to duplicate or renumber a file descriptor to an arbitrary number, like dup2(). 
+/// This would be prone to race conditions, as an actual file descriptor with the same number could be allocated by a different thread at the same time. 
+/// This function provides a way to atomically renumber file descriptors, which would disappear if dup2() were to be removed entirely.
 fn fd_renumber(fd: Fd, to: Fd) -> Result<(), Errno> { unimplemented!() }
-/// fd_seek(fd: fd, offset: filedelta, whence: whence) -> Result<filesize, errno>
+
+/// Move the offset of a file descriptor. 
+/// Note: This is similar to lseek in POSIX.
 fn fd_seek(fd: Fd, offset: Filedelta, whence: Whence) -> Result<Filesize, Errno> { unimplemented!() }
-/// fd_sync(fd: fd) -> Result<(), errno> 
+
+/// Synchronize the data and metadata of a file to disk. 
+/// Note: This is similar to fsync in POSIX.
 fn fd_sync(fd: Fd) -> Result<(), Errno>  { unimplemented!() }
-/// fd_tell(fd: fd) -> Result<filesize, errno>
+
+/// Return the current offset of a file descriptor. 
+/// Note: This is similar to lseek(fd, 0, SEEK_CUR) in POSIX.
 fn fd_tell(fd: Fd) -> Result<Filesize, Errno> { unimplemented!() }
-/// fd_write(fd: fd, iovs: ciovec_array) -> Result<size, errno>
+
+/// Write to a file descriptor. 
+/// Note: This is similar to writev in POSIX.
 fn fd_write(fd: Fd, iovs: CiovecArray) -> Result<Size, Errno> { unimplemented!() }
-/// path_create_directory(fd: fd, path: string) -> Result<(), errno>
+
+/// Create a directory. 
+/// Note: This is similar to mkdirat in POSIX.
 fn path_create_directory(fd: Fd, path: String) -> Result<(), Errno> { unimplemented!() }
-/// path_filestat_get(fd: fd, flags: lookupflags, path: string) -> Result<filestat, errno> 
+
+/// Return the attributes of a file or directory. 
+/// Note: This is similar to stat in POSIX.
 fn path_filestat_get(fd: Fd, flags: Lookupflags, path: String) -> Result<Filestat, Errno> { unimplemented!() }
-/// path_filestat_set_times(fd: fd, flags: lookupflags, path: string, atim: timestamp, mtim: timestamp, fst_flags: fstflags) -> Result<(), errno>
+
+/// Adjust the timestamps of a file or directory. 
+/// Note: This is similar to utimensat in POSIX.
 fn path_filestat_set_times(fd: Fd, flags: Lookupflags, path: String, atim: Timestamp, mtim: Timestamp, fst_flags: Fstflags) -> Result<(), Errno> { unimplemented!() }
-/// path_link(old_fd: fd, old_flags: lookupflags, old_path: string, new_fd: fd, new_path: string) -> Result<(), errno>
+
+/// Create a hard link. 
+/// Note: This is similar to linkat in POSIX.
 fn path_link(old_fd: Fd, old_flags: Lookupflags, old_path: String, new_fd: Fd, new_path: String) -> Result<(), Errno> { unimplemented!() }
-/// path_open(fd: fd, dirflags: lookupflags, path: string, oflags: oflags, fs_rights_base: rights, fs_rights_inheriting: rights, fdflags: fdflags) -> Result<fd, errno>
+
+/// Open a file or directory. 
+/// The returned file descriptor is not guaranteed to be the lowest-numbered file descriptor not currently open; 
+/// it is randomized to prevent applications from depending on making assumptions about indexes, since this is error-prone in multi-threaded contexts. 
+/// The returned file descriptor is guaranteed to be less than 2**31. 
+/// Note: This is similar to openat in POSIX.
 fn path_open(fd: Fd, dirflags: Lookupflags, path: String, oflags: Oflags, fs_rights_base: Rights, fs_rights_inheriting: Rights, fdflags: Fdflags) -> Result<Fd, Errno> { unimplemented!() }
-/// path_readlink(fd: fd, path: string, buf: Pointer<u8>, buf_len: size) -> Result<size, errno>
+
+/// Read the contents of a symbolic link. 
+/// Note: This is similar to readlinkat in POSIX.
 fn path_readlink(fd: Fd, path: String, buf: *mut u8, buf_len: Size) -> Result<Size, Errno> { unimplemented!() }
-/// path_remove_directory(fd: fd, path: string) -> Result<(), errno>
+
+/// Remove a directory. 
+/// Return errno::notempty if the directory is not empty. 
+/// Note: This is similar to unlinkat(fd, path, AT_REMOVEDIR) in POSIX.
 fn path_remove_directory(fd: Fd, path: String) -> Result<(), Errno> { unimplemented!() }
-/// path_rename(fd: fd, old_path: string, new_fd: fd, new_path: string) -> Result<(), errno>
+
+/// Rename a file or directory. 
+/// Note: This is similar to renameat in POSIX.
 fn path_rename(fd: Fd, old_path: String, new_fd: Fd, new_path: String) -> Result<(), Errno> { unimplemented!() }
-/// path_symlink(old_path: string, fd: fd, new_path: string) -> Result<(), errno>
+
+/// Create a symbolic link. 
+/// Note: This is similar to symlinkat in POSIX.
 fn path_symlink(old_path: String, fd: Fd, new_path: String) -> Result<(), Errno> { unimplemented!() }
-/// path_unlink_file(fd: fd, path: string) -> Result<(), errno>
+
+/// Unlink a file. 
+/// Return errno::isdir if the path refers to a directory. 
+/// Note: This is similar to unlinkat(fd, path, 0) in POSIX.
 fn path_unlink_file(fd: Fd, path: String) -> Result<(), Errno> { unimplemented!() }
-/// poll_oneoff(in: ConstPointer<subscription>, out: Pointer<event>, nsubscriptions: size) -> Result<size, errno>
+
+/// Concurrently poll for the occurrence of a set of events.
 fn poll_oneoff(in_: *const Subscription, out: *mut Event, nsubscriptions: Size) -> Result<Size, Errno> { unimplemented!() }
-/// proc_exit(rval: exitcode)
+
+/// Terminate the process normally. 
+/// An exit code of 0 indicates successful termination of the program. 
+/// The meanings of other values is dependent on the environment.
 fn proc_exit(rval: Exitcode) { unimplemented!() }
-/// proc_raise(sig: signal) -> Result<(), errno>
+
+/// Send a signal to the process of the calling thread. 
+/// Note: This is similar to raise in POSIX.
 fn proc_raise(sig: Signal) -> Result<(), Errno> { unimplemented!() }
-/// sched_yield() -> Result<(), errno> 
+
+/// Temporarily yield execution of the calling thread. 
+/// Note: This is similar to sched_yield in POSIX.
 fn sched_yield() -> Result<(), Errno> { unimplemented!() }
-/// random_get(buf: Pointer<u8>, buf_len: size) -> Result<(), errno>
+
+/// Write high-quality random data into a buffer. 
+/// This function blocks when the implementation is unable to immediately provide sufficient high-quality random data. 
+/// This function may execute slowly, so when large mounts of random data are required, it's advisable to use this function to seed a pseudo-random number generator, rather than to provide the random data directly.
 fn random_get(buf: *mut u8, buf_len: Size) -> Result<(), Errno> { unimplemented!() }
-/// sock_recv(fd: fd, ri_data: iovec_array, ri_flags: riflags) -> Result<(size, roflags), errno>
+
+/// Receive a message from a socket. 
+/// Note: This is similar to recv in POSIX, though it also supports reading the data into multiple buffers in the manner of readv.
 fn sock_recv(fd: Fd, ri_data: IovecArray, ri_flags: Riflags) -> Result<(Size, Roflags), Errno> { unimplemented!() }
-/// sock_send(fd: fd, si_data: ciovec_array, si_flags: siflags) -> Result<size, errno>
+
+/// Send a message on a socket. 
+/// Note: This is similar to send in POSIX, though it also supports writing the data from multiple buffers in the manner of writev.
 fn sock_send(fd: Fd, si_data: CiovecArray, si_flags: Siflags) -> Result<Size, Errno> { unimplemented!() }
-/// sock_shutdown(fd: fd, how: sdflags) -> Result<(), errno>
+
+/// Shut down socket send and receive channels. 
+/// Note: This is similar to shutdown in POSIX.
 fn sock_shutdown(fd: Fd, how: Sdflags) -> Result<(), Errno> { unimplemented!() }
